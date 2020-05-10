aea5ed18dc40979d1812a75b841ca6ac095a4ac1
braindead1 <2136249+braindead1@users.noreply.github.com>
2020-05-10 21:28:00 +0200

    Fix for Sentry error IOBROKER-UNIFI-1F

--- main.js
+++ main.js
@@ -207,6 +207,8 @@ class Unifi extends utils.Adapter {
 
                 if (err.message === 'api.err.Invalid') {
                     this.log.error('Error: Incorrect username or password.');
+                } else if (err.message.includes('connect ECONNREFUSED') === true) {
+                    this.log.error('Error: Connection refused. Incorrect IP or port.');
                 } else {
                     this.errorHandling(err);
                 }
