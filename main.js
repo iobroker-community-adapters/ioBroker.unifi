aca30012271a6721e2d257fadc281c6c661b643f
braindead1 <2136249+braindead1@users.noreply.github.com>
2020-06-26 19:50:42 +0200

    Renamed gatewayTraffic

--- main.js
+++ main.js
@@ -758,7 +758,7 @@ class Unifi extends utils.Adapter {
      * @param {Object} data 
      */
     async processGatewayTraffic(sites, data) {
-        const objects = require('./admin/lib/objects_gatewayTraffic.json');
+        const objects = require('./admin/lib/objects_gateway_traffic.json');
 
         for (const site of sites) {
             const x = sites.indexOf(site);
@@ -772,7 +772,7 @@ class Unifi extends utils.Adapter {
             });
 
             if (siteData.length > 0) {
-                await this.applyJsonLogic(site, siteData, objects, this.statesFilter.gatewayTraffic);
+                await this.applyJsonLogic(site, siteData, objects, this.statesFilter.gateway_traffic);
             }
         }
     }
