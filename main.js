a5e42d64299d85546aae7726fbf53a14e62f529f
braindead1 <2136249+braindead1@users.noreply.github.com>
2020-06-29 16:55:22 +0200

    Fix for Sentry error IOBROKER-UNIFI-33

--- main.js
+++ main.js
@@ -170,6 +170,8 @@ class Unifi extends utils.Adapter {
             this.log.error('Error: Incorrect username or password.');
         } else if (err.message === 'api.err.LoginRequired') {
             this.log.error('Error: Login required. Check username and password.');
+        } else if (err.message === 'api.err.Ubic2faTokenRequired') {
+            this.log.error('Error: 2-Factor-Authentication required by UniFi controller. 2FA is not supported by this adapter.');
         } else if (err.message.includes('connect ECONNREFUSED') === true) {
             this.log.error('Error: Connection refused. Incorrect IP or port.');
         } else if (err.message.includes('read ECONNRESET') === true) {
